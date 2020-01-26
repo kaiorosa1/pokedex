2e466be52bb7aaa223eeac33e6768b9fdc574832
kaiorosa1 <kaiosrosa@gmail.com>
2020-01-25 22:15:04 -0300

    add components

---
+++ src/containers/Pokecard/Pokecard.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+class Pokecard extends React.Component {
+    
+    render(){
+        return (
+            <div>
+
+            </div>
+        );
+    }
+}
+
+
+export default Pokecard;
