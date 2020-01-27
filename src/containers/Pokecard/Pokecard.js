e56d23678cbb748ccb4c91f2a5a26d735279ce2c
kaiorosa1 <kaiosrosa@gmail.com>
2020-01-26 23:02:12 -0300

    add img tag not working

--- src/containers/Pokecard/Pokecard.js
+++ src/containers/Pokecard/Pokecard.js
@@ -7,7 +7,9 @@ class Pokecard extends React.Component {
             <div className="Pokecard tc bg-moon-gray dib pa3 ma2 grow bw2 shadow-5">
                 {name}
                 <br />
-                {type}
+                {/* <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${key}.png.`} alt={name}/>
+                 */}
+                 {type}
                 <br />
                 {base_experience}
                 <br />
