0a1fa64225cb238a6bd1aca12ec14e6df0b314f2
kaiorosa1 <kaiosrosa@gmail.com>
2020-01-26 22:47:17 -0300

    add cards

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import './App.css';
 import Pokedex from './Pokedex/Pokedex';
+import 'tachyons';
+import cards from '../pokemons.js';
 
 function App() {
   return (
     <div className="App">
       <h1>Pokedex</h1>
-      <Pokedex />
+      <div className="mw9 center ph3-ns">
+        <div className="cf ph2-ns">
+          <Pokedex cards={cards}/>
+        </div>
+      </div>
     </div>
   );
 }
--- src/containers/Pokedex/Pokedex.js
+++ src/containers/Pokedex/Pokedex.js
@@ -2,13 +2,20 @@ import React from 'react';
 import Pokecard from '../Pokecard/Pokecard';
 
 class Pokedex extends React.Component {
-    
     render(){
-        return (
-            <div>
-                <Pokecard name="Charmander" type="fire" base_experience={62}/>
-            </div>
-        );
+        const cardArray = this.props.cards.map((card) => {
+            return (
+                    <div class="fl w-100 w-third-ns pa2">
+                        <Pokecard key={card.id} 
+                            name={card.name}
+                            type={card.type} 
+                            base_experience={card.base_experience}
+                        />
+                    </div>
+            );
+        })
+        return cardArray;
+        
     }
 }
 
