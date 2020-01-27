9a84e016fb410064e110b12b994e043b4e71da2f
kaiorosa1 <kaiosrosa@gmail.com>
2020-01-26 22:51:44 -0300

    add pokecard css

--- src/containers/Pokecard/Pokecard.js
+++ src/containers/Pokecard/Pokecard.js
@@ -4,7 +4,7 @@ class Pokecard extends React.Component {
     render(){
         const {name, type, base_experience} = this.props;
         return (
-            <div className="Pokecard">
+            <div className="Pokecard tc bg-moon-gray dib pa3 ma2 grow bw2 shadow-5">
                 {name}
                 <br />
                 {type}
