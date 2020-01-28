31e4538fc34cdab95cd4ec22cf5f300cc9a32c83
kaiorosa1 <kaiosrosa@gmail.com>
2020-01-27 21:26:23 -0300

    remove Pokecard class

--- src/containers/Pokecard/Pokecard.js
+++ src/containers/Pokecard/Pokecard.js
@@ -4,7 +4,7 @@ class Pokecard extends React.Component {
     render(){
         const {name, type, base_experience} = this.props;
         return (
-            <div className="Pokecard tc bg-moon-gray dib pa3 ma2 grow bw2 shadow-5">
+            <div className="tc bg-moon-gray dib pa3 ma2 grow bw2 shadow-5">
                 {name}
                 <br />
                 {/* <img src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/${key}.png.`} alt={name}/>
